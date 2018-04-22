e96391785dd6bde261067c85d1231fe1176e838b
shhj1998 <shhj1998@postech.ac.kr>
2018-04-22 12:03:27 +0900

    failed to prove 6086

--- baekjoon/6086.cpp
+++ baekjoon/6086.cpp
@@ -8,28 +8,31 @@ using namespace std;
 
 int vertex(char ch){
     if(ch >= 'a')
-        return 'Z' + ch - 'a';
+        return 'Z' - 'A' + ch - 'a' + 1;
     else
         return ch - 'A';
 }
 
 int n;
 vector<vector<int> > graph(vertex('z') + 1);
-int Capacity[102][102];
-int Flow[102][102];
+vector<bool> visit(60, false);
+int Capacity[60][60];
+int Flow[60][60];
 
 int get_flow(int u, int MaxCapacity){
     if(u == vertex('Z'))
         return MaxCapacity;
     for(int next : graph[u]){
-        int c = Capacity[u][next] - Flow[u][next];
-        if(int f = get_flow(next, min(MaxCapacity, c)) > 0){
-            Flow[u][next] += f;
-            return f;
-        }
-        c = Flow[next][u];
-        if(int f = get_flow(next, min(MaxCapacity, c)) > 0){
-            Flow[next][u] -= f;
+        if(visit[next])
+            continue;
+
+        visit[next] = true;
+        int c = Flow[next][u] ? Flow[next][u] + Capacity[next][u] : Capacity[u][next] - Flow[u][next];
+        int f = get_flow(next, min(MaxCapacity, c));
+
+        if(f > 0){
+            Flow[u][next] += max(0, f - Flow[next][u]);
+            Flow[next][u] = max(0, Flow[next][u] - f);
             return f;
         }
     }
@@ -38,9 +41,14 @@ int get_flow(int u, int MaxCapacity){
 }
 
 int get_max_flow(){
-    int total = 0;
-    while(int f = get_flow(vertex('A'), 1e9) > 0){
+    int total = 0, f;
+    f = get_flow(vertex('A'), 1e9);
+
+    while(f > 0){
         total += f;
+        for(int i = 1; i < 60; i++)
+            visit[i] = false;
+        f = get_flow(vertex('A'), 1e9);
     }
     return total;
 }
@@ -52,12 +60,13 @@ int main(){
         char u, v;
         int c;
         cin >> u >> v >> c;
-        Capacity[vertex(u)][vertex(v)] = c;
-        Capacity[vertex(u)][vertex(v)] = c;
+        Capacity[vertex(u)][vertex(v)] += c;
+        Capacity[vertex(v)][vertex(u)] += c;
         graph[vertex(u)].push_back(vertex(v));
         graph[vertex(v)].push_back(vertex(u));
     }
 
+    visit[vertex('A')] = true;
     cout << get_max_flow() << '\n';
 
     return 0;
