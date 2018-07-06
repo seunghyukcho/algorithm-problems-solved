19cc6fba5b927b9b3dcf99ad7db45d6ebddb6289
choseunghyuk <choseunghyek@gmail.com>
2018-07-07 00:01:30 +0900

    solved skill tree

--- baekjoon/15309.cpp
+++ baekjoon/15309.cpp
@@ -1,89 +1,67 @@
-#include <iostream>
+#include<iostream>
 
 using namespace std;
 
 typedef long long ll;
 
-ll P = 1000000007;
+ll A, B, N, x, y, m;
+ll MOD = 1e9 + 7;
 
-int A, B;
-
-ll pow(ll a, ll b)
-{
-    ll r = 1;
-    while(b > 0)
-    {
-        if(b % 2)
-        {
-            r *= a;
-            r %= P;
-        }
-        a *= a;
-        a %= P;
-        b /= 2;
-    }
-    return r % P;
+ll mod(ll a) {
+    if(a < 0) a += MOD;
+    return a % MOD;
 }
 
-ll price(ll x, ll y, ll m)
-{
-    ll k = pow(A, (x - y)) * pow(B, y - 1);
-    k %= P;
-    ll s;
-    if(A > B)
-    {
-        ll C = pow((A - B), P - 2);
-        ll D = (pow(A, m+1) - 1) * pow((A-1), P - 2);
-        D %= P;
-        ll E = (pow(B, m+1) - 1) * pow((B-1), P - 2);
-        E %= P;
-        s = (C * ((D - E + P) % P)) % P;
-    }
-    else if(A < B)
-    {
-        ll C = pow((B - A), P - 2);
-        ll D = (pow(A, m+1) - 1) * pow((A-1), P - 2);
-        D %= P;
-        ll E = (pow(B, m+1) - 1) * pow((B-1), P - 2);
-        E %= P;
-        s = (C * ((E - D + P) % P)) % P;
-    }
-    else
-    {
-        if(A == 1)
-        {
-            s = (m % P) * ((m + 1) % P);
-            s %= P;
-            s *= pow(2, P - 2);
-            s %= P;
-        }
-        else
-        {
-            ll C = pow((A - 1), P - 2);
-            C %= P;
-            ll D = (m % P) * pow(A, m);
-            D %= P;
-            ll E = (pow(A, m) - 1) * pow((A-1), P - 2);
-            E %= P;
-            s = C * ((D - E + P) % P);
-            s %= P;
-        }
-    }
-    ll r = k * s;
-    return r % P;
+ll mul(ll a, ll b) {
+    return mod(mod(a) * mod(b));
 }
 
-int main()
-{
-    scanf("%d%d", &A, &B);
+ll pow(ll a, ll b) {
+    if(b == 0) return 1;
 
-    int N;
-    scanf("%d", &N);
-    for(int i = 0; i < N; i++)
-    {
-        ll x, y, m;
+    ll ret = pow(a, b / 2);
+    ret = mul(ret, ret);
+    if(b % 2) ret = mul(ret, a);
+
+    return mod(ret);
+}
+
+ll inv(ll a, ll b) {
+    return mul(mod(a), mod(pow(b, MOD - 2)));
+}
+
+ll f0() {
+    return mul(pow(A, x - y), pow(B, y - 1));
+}
+
+ll f1() {
+    if(A == 1) return inv(mul(m, m + 1), 2);
+    else return mod(inv(mul(m, pow(A, m)), A - 1) - inv((pow(A, m) - 1), pow(A - 1, 2)));
+}
+
+ll f2() {
+    ll left, right;
+
+    if(A == 1) left = m + 1;
+    else left = inv(pow(A, m + 1) - 1, A - 1);
+
+    if(B == 1) right = m + 1;
+    else right = inv(pow(B, m + 1) - 1, B - 1);
+
+    return inv(left - right, A - B);
+}
+
+int main(){
+    scanf("%lld%lld", &A, &B);
+
+    for(scanf("%lld", &N); N > 0; N--) {
+        ll ans;
         scanf("%lld%lld%lld", &x, &y, &m);
-        printf("%lld\n", price(x, y, m));
+
+        if(A == B) ans = mul(f0(), f1());
+        else ans = mul(f0(), f2());
+
+        printf("%lld\n", ans);
     }
 
     return 0;
