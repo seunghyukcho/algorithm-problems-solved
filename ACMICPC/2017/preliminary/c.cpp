f1208755fb7f702cd8f7b48117f73e68bb236d71
choseunghyuk <choseunghyek@gmail.com>
2018-10-01 15:12:19 +0900

    - lca coding - acmicpc 2017 대전 인터넷 예선 수정

--- ACMICPC/2017/preliminary/c.cpp
+++ ACMICPC/2017/preliminary/c.cpp
@@ -1,123 +1,124 @@
 #include<iostream>
 #include<stack>
+#include<vector>
 #include<string>
 using namespace std;
 
 int main()
 {
-    stack<char> sp;
-    int w;
-    string input;
-    cin >> w >> input;
-
-    bool is_end = false;
-    char last;
-    bool strange = false;
-    int v = 0, ef = 0, eb = 0;
-    for(int i = 0; i < input.size(); i++)
+  int w;
+  stack<char> sp;
+  string input;
+  cin >> w;
+  getline(cin, input);
+  getline(cin, input);
+  int v = 0, ef = 0, eb = 0;
+  bool is_end = true;
+  bool is_strange = false;
+  char last = 'S';
+  for(int i = 0 ;i < input.size(); i++)
+  {
+    if(input[i] == ' ')
     {
-        if(input[i] == ' ') continue;
-        else if(input[i] == 'S')
-        {
-            if(is_end == true)
-            {
-                printf("s strange\n");
-                strange = true;
-                break;
-            }
-            is_end = true;
-            v++;
-            ef++;
-            last = input[i];
-        }
-        else if(input[i] == 'L')
-        {
-            if(is_end == true)
-            {
-                printf("l strange\n");
-                printf("%d\n", i);
-                strange = true;
-                break;
-            }
-            is_end = true;
-            v++;
-            ef+=2;
-            eb+=1;
-            last = input[i];
-        }
-        else if(input[i] == 'B')
-        {
-            if(is_end == true)
-            {
-                printf("b strange\n");
-                strange = true;
-                break;
-            }
-            is_end = true;
-            v++;
-            ef++;
-            last = input[i];
-        }
-        else if(input[i] == ',')
-        {
-            if(is_end == false)
-            {
-                printf(", strange\n");
-                strange = true;
-                break;
-            }
-            is_end = false;
-            last = input[i];
-        }
-        else if(i && (input[i] == '(' || input[i] == '['))
-        {
-            if(last != 'L' && last != 'B')
-            {
-                printf("(, [ strange\n");
-                printf("%d %c\n", i, last);
-                strange = true;
-                break;
-            }
-            sp.push(input[i]);
-            is_end = false;
-            last = input[i];
-        }
-        else if(i && input[i] == ')')
-        {
-            if(sp.top() != '(')
-            {
-                printf(") strange\n");
-                strange = true;
-                break;
-            }
-            v++;
-            sp.pop();
-            last = input[i];
-        }
-        else if(i && input[i] == ']')
-        {
-            if(sp.top() != '[')
-            {
-                printf("] strange\n");
-                strange = true;
-                break;
-            }
-            v++;
-            sp.pop();
-            last = input[i];
-        }
-        else
-        {
-            printf("string strange\n");
-            strange = true;
-            break;
-        }
+      continue;
     }
-    if(strange)
+    else if(input[i] == 'S')
     {
-        printf("-1\n");
-        return 0;
+      if(!is_end)
+      {
+        is_strange = true;
+        break;
+      }
+      v+=1;
+      ef+=1;
+      is_end = false;
+      last = input[i];
     }
-    printf("%d %d %d %d\n", ef, eb, v, w);
-    printf("%d\n", ef+(w*eb)+2-v);
+    else if(input[i] == 'L')
+    {
+      if(!is_end)
+      {
+        is_strange = true;
+        break;
+      }
+      v+=2;
+      ef+=2;
+      eb+=1;
+      is_end = false;
+      last = input[i];
+    }
+    else if(input[i] == 'B')
+    {
+      if(!is_end)
+      {
+        is_strange = true;
+        break;
+      }
+      v+=2;
+      ef+=3;
+      is_end = false;
+      last = input[i];
+    }
+    else if(input[i] == ',')
+    {
+      if(is_end)
+      {
+        is_strange = true;
+        break;
+      }
+      is_end = true;
+    }
+    else if(i && input[i] == '(')
+    {
+      if(last != 'L' && last != 'B')
+      {
+        is_strange = true;
+        break;
+      }
+      sp.push(input[i]);
+      is_end = true;
+      last = 'S';
+    }
+    else if(i && input[i] == '[')
+    {
+      if(last != 'L' && last != 'B')
+      {
+        is_strange = true;
+        break;
+      }
+      sp.push(input[i]);
+      is_end = true;
+      last = 'S';
+    }
+    else if(input[i] == ')')
+    {
+      if(sp.size() == 0 || sp.top() != '(')
+      {
+        is_strange = true;
+        break;
+      }
+      sp.pop();
+    }
+    else if(input[i] == ']')
+    {
+      if(sp.size() == 0 || sp.top() != '[')
+      {
+        is_strange = true;
+        break;
+      }
+      sp.pop();
+    }
+    else
+    {
+      is_strange = true;
+      break;
+    }
+  }
+  if(is_strange || is_end || sp.size())
+  {
+    printf("-1\n");
+    return 0;
+  }
+  printf("%d\n", ef+(w*eb)-v+1);
+  return 0;
 }
--- baekjoon/11438.cpp
+++ baekjoon/11438.cpp
@@ -1,5 +1,122 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<queue>
 
 using namespace std;
+
+const int MAXV = 100002, MAXE = 100002;
+struct Tree {
+    struct Edge {
+        int idx, start, end, cost;
+    };
+    struct Vertex {
+        int parent, level;
+        vector<int> connected;
+    } V[MAXV];
+
+    int n, root, sparse[MAXV][100];
+    vector<Edge> E;
+
+    void setEdge(int u, int v, int c) {
+        int idx = E.size();
+        Edge edge1, edge2;
+
+        edge1.idx = idx, edge2.idx = idx + 1;
+        edge1.start = edge2.end = u;
+        edge1.end = edge2.start = v;
+        edge1.cost = edge2.cost = c;
+
+        V[u].connected.push_back(idx);
+        V[v].connected.push_back(idx + 1);
+        E.push_back(edge1);
+        E.push_back(edge2);
+    }
+
+    void setRoot(int root) {
+        this->root = root;
+        V[root].level = 0;
+        setRootedTree();
+    }
+
+    void setRootedTree() {
+        queue<int> q;
+        q.push(root);
+
+        while(!q.empty()) {
+            int here = q.front(); q.pop();
+
+            for(auto edge : V[here].connected) {
+                int next = E[edge].end;
+
+                if(next != V[here].parent) {
+                    V[next].parent = here;
+                    V[next].level = V[here].level + 1;
+                    q.push(next);
+                }
+            }
+        }
+    }
+
+    int table(int v, int n) {
+        if(sparse[v][n]) return sparse[v][n];
+
+        return sparse[v][n] = (n == 0 ? V[v].parent : table(table(v, n - 1), n - 1));
+    }
+
+    int findParent(int v, int n) {
+        for(int i = 0; (1 << i) <= n; i++)
+            if(n & (1 << i)) v = table(v, i);
+
+        return v;
+    }
+
+    int LCA(int v1, int v2) {
+        if(v1 == v2) return v1;
+        if(V[v1].level > V[v2].level) swap(v1, v2);
+
+        int diff = V[v2].level - V[v1].level;
+        v2 = findParent(v2, diff);
+
+        if(v1 == v2) return v1;
+
+        int start = 1, end = V[v1].level;
+        while(start < end) {
+            int mid = (start + end) / 2;
+
+            int p1 = findParent(v1, mid), p2 = findParent(v2, mid);
+            if(p1 == p2)
+                end = mid;
+            else {
+                end -= mid;
+                v1 = p1, v2 = p2;
+            }
+        }
+
+        return findParent(v1, start);
+    }
+} T;
+
+int m;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> T.n;
+    for(int i = 0; i < T.n - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        T.setEdge(u, v, 1);
+    }
+    T.setRoot(1);
+
+    for(cin >> m; m > 0; m--) {
+        int u, v;
+        cin >> u >> v;
+
+        cout << T.LCA(u, v) << '\n';
+    }
+
+    return 0;
+}
