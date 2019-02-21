f71609c3fb602f865164bdd7382d99e572cb7a8e
shhj1998 <choseunghyek@gmail.com>
2019-02-21 13:16:40 +0900

    solved d.cpp

--- atcoder/beginner/118/D.cpp
+++ atcoder/beginner/118/D.cpp
@@ -8,34 +8,56 @@ int n, m, a[10], cnt[10] = {0, 2, 5, 5, 4, 5, 6, 3, 7, 6};
 int d[10002];
 string ans[10002];
 
-string dy(int n)
+int dy(int n)
 {
+	if(n < 0) return -2;
+	
 	int& ret = d[n];
+	string& s = ans[n];
+
 	int here = 0;
 	if(ret != -1) return ret;
 
 	for(int i = 1; i <= m; i++)
 	{
-		if(ret < dy(n - cnt[a[i]]))
+		int next = dy(n - cnt[a[i]]);
+
+		if(next == -2) continue;
+		if(ret < next)
 		{
 			here = i;
-			ret = dy(n - cnt[a[i]]);
+			ret = next;
 		}
-		else if(ret == dy(n - cnt[a[i]]) && a[i] > a[here])
+		else if(ret == next && a[i] > a[here])
 		{
 			here = i;
 		}
 	}
 
 	ret = (ret == -1 ? -2 : ret + 1);
-	ans[n] = (ret == -2 ? "" : ans[n - cnt[a[here]]] + )
+	if(ret != -2)
+	{
+		char ch = a[here] + '0';
+		s = ans[n - cnt[a[here]]];
+		s.push_back(ch);
+	}
+
+	return ret;
 }
 
 int main()
 {
 	cin >> n >> m;
-	for(int i = 0; i < m; i++) cin >> a[i];
-	for(int i = 0; i <= n; i++) d[i] = -1;
-	
+	for(int i = 1; i <= m; i++) cin >> a[i];
+	for(int i = 0; i <= n; i++) 
+	{
+		d[i] = -1;
+		ans[i] = "";
+	}
 
+	d[0] = 0;
+	
+	dy(n);
+	reverse(ans[n].begin(), ans[n].end());
+	cout << ans[n] << '\n';
 }
