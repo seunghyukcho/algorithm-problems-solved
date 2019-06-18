d651852ed2af009a9da06d6422023df12c969db5
seunghyek.cho <shhj1998@postech.ac.kr>
2019-06-18 14:34:18 +0900

    hackercup qual

--- facebook/2019/Qualification Round/C.cpp
+++ facebook/2019/Qualification Round/C.cpp
@@ -1,23 +1,13 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 
 using namespace std;
 
 pair<int, int> ans(string &s, int pos)
 {
 	if(s[pos] != '(')
-	{
-		pair<int, int> ret = {0, pos + 1};
-		
-		if(s[pos] == '0' || s[pos] == '1')
-			ret.first = s[pos] - '0';
-		else if(s[pos] == 'x')
-			ret.first = 3;
-		else
-			ret.first = 4;
-
-		return ret;
-	}
+		return {s[pos] - '0', pos + 1};
 
 	auto result1 = ans(s, pos + 1);
 	auto result2 = ans(s, result1.second + 1);
@@ -26,16 +16,19 @@ pair<int, int> ans(string &s, int pos)
 	int right = result2.first;
 	char op = s[result1.second];
 
-	pair<int, int> ret = {2, result2.second + 1};
-	if(op == '|')
-	{
-		if(right <= 1 && left <= 1)
-			ret.first = right | left;
-		else if(right == 1 || left == 1)
-			ret.first = 1;
-		else if(right + left == 7)
-			ret.first = 1;
+	pair<int, int> ret = {0, result2.second + 1};
 
+	switch(op)
+	{
+		case '|':
+			ret.first = left | right;
+			break;
+		case '&':
+			ret.first = left & right;
+			break;
+		case '^':
+			ret.first = left ^ right;
+			break;
 	}
 
 	return ret;
@@ -48,13 +41,27 @@ int main()
 
 	for(t = 1; t <= T; t++)
 	{
-		string s;
+		string s, s1, s2;
 
 		cout << "Case #" << t << ": ";
 		cin >> s;
+		s1 = s2 = s;
+		for(int i = 0; i < s.size(); i++)
+		{
+			if(s1[i] == 'x')
+			{
+				s1[i] = '0';
+				s2[i] = '1';
+			}
+			else if(s1[i] == 'X')
+			{
+				s1[i] = '1';
+				s2[i] = '0';
+			}
+		}
 
-		auto ret = ans(s, 0);
-		cout << ret.first << ' ' << ret.second << '\n';
+		auto ret1 = ans(s1, 0), ret2 = ans(s2, 0);
+		cout << abs(ret1.first - ret2.first) << '\n';
 	}
 
 	return 0;
